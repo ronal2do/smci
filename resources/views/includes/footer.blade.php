142f73f2d0b69555a9b07cb0a23cf3480e2001d5
Sotaque Dev <sotaquedev@SOTMAC14.local>
2016-05-16 17:36:54 -0300

    mudancas 16-05 2

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,27 +1,25 @@
 
 <footer class="footer">
   <div class="container">
-   <div class="row">Realização</div>
+
     <div class="columns">
-      <div class="column is-half">
-         
-          <figure class="image is-half" style="max-width: 356px;">
-            <img src="/frontend/imagens/realizacao.jpg">
+      <div class="column">
+         Organização:
+          <figure class="image is-half" style="max-width: 200px;height:165px;">
+            <img src="/frontend/imagens/publico.png">
             forum
           </figure>
      
       </div>
-      <div class="column">
-           <figure class="image has-text-centered" style="max-width: 149px; margin-top: 10px;">
-            <img src="/frontend/imagens/antp.jpg">
-            ANTP
-          </figure>
+      <div class="column is-half">
+      Apoio:
+         
   
       </div>
        <div class="column is-centered">
-          <figure class="image is-centered" style="max-width: 96px;margin-top: -43px;">
-            <img src="/frontend/imagens/cosems.png">
-          </figure>
+       Entidades Apoiadoras: <br><br>
+       <strong>ANTP</strong>   - Fórum Paulista de Secretários e Dirigentes Públicos de Mobilidade Urbana <br>
+       <strong>COSEMS - SP</strong>    - Conselho de Secretários Municipais de Saúde do Estado de São Paulo 
   
       </div>
  
