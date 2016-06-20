0822b8749e0d59e0d8c75eacb8e7abfaffffbc1c
System Administrator <root@SOTMAC14.local>
2016-06-20 15:05:24 -0300

    roadape

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -4,23 +4,36 @@
 
     <div class="columns">
       <div class="column">
-         Organização:
+         Realização:
           <figure class="image is-half" style="max-width: 200px;height:165px;">
-            <img src="/frontend/imagens/publico.png">
+            <!-- <img src="/frontend/imagens/publico.png"> -->
+            <img src="/frontend/imagens/antp.jpg">
             forum
           </figure>
-     
       </div>
       <div class="column is-half">
-      Apoio:
-         
+      <!-- Apoio: -->
+      <br>
+           <figure class="image is-half" style="max-width: 400px;height:165px;">
+                        <img src="/frontend/imagens/realizacao.jpg">
+            forum
+          </figure>
   
       </div>
-       <div class="column is-centered">
+
+     <!--   <div class="column is-centered">
        Entidades Apoiadoras: <br><br>
        <strong>ANTP</strong>   - Fórum Paulista de Secretários e Dirigentes Públicos de Mobilidade Urbana <br>
        <strong>COSEMS - SP</strong>    - Conselho de Secretários Municipais de Saúde do Estado de São Paulo 
   
+      </div> -->
+      <div class="column is-centered">
+        <br>
+          <figure class="image is-half" style="max-width: 70px;height:165px;">
+            <!-- <img src="/frontend/imagens/publico.png"> -->
+            <img src="/frontend/imagens/cosems.png">
+            forum
+          </figure>
       </div>
  
     </div>
