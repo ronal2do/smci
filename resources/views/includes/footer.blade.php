b904cab7e963ec0240df4f3d4f97d9e44439592b
System Administrator <root@SOTMAC14.local>
2016-06-29 10:44:29 -0300

    assinaturas

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -3,36 +3,43 @@
   <div class="container">
 
     <div class="columns">
-      <div class="column">
+      <div class="column is-1">
          Realização:
-          <figure class="image is-half" style="max-width: 200px;height:165px;">
+          <figure class="image" style="max-width: 64px;height:165px;">
             <!-- <img src="/frontend/imagens/publico.png"> -->
-            <img src="/frontend/imagens/antp.jpg">
-            forum
+            <img src="/frontend/imagens/realizacao1.png">
+          
           </figure>
       </div>
-      <div class="column is-half">
-      <!-- Apoio: -->
+       <div class="column is-5">
+   <br>
+          <figure class="image" style="max-width: 416px;height:165px;">
+            <!-- <img src="/frontend/imagens/publico.png"> -->
+            <img src="/frontend/imagens/realizacao2.png">
+          
+          </figure>
+      </div>
+      <div class="column is-2">
+      Apoio:
       <br>
-           <figure class="image is-half" style="max-width: 400px;height:165px;">
-                        <img src="/frontend/imagens/realizacao.jpg">
-            forum
+           <figure class="image is-centered" style="max-width: 160px;height:165px;">
+                        <img src="/frontend/imagens/apoio.png">
+          
           </figure>
   
       </div>
-
      <!--   <div class="column is-centered">
        Entidades Apoiadoras: <br><br>
        <strong>ANTP</strong>   - Fórum Paulista de Secretários e Dirigentes Públicos de Mobilidade Urbana <br>
        <strong>COSEMS - SP</strong>    - Conselho de Secretários Municipais de Saúde do Estado de São Paulo 
   
       </div> -->
-      <div class="column is-centered">
-        <br>
-          <figure class="image is-half" style="max-width: 70px;height:165px;">
+      <div class="column is-4">
+         Patrocínio:
+          <figure class="image is-half" style="max-width: 465px;height:165px;">
             <!-- <img src="/frontend/imagens/publico.png"> -->
-            <img src="/frontend/imagens/cosems.png">
-            forum
+            <img src="/frontend/imagens/patrocinio.png">
+          
           </figure>
       </div>
  
