0fdb62fd2c324e4a3853651b59ad024181248024
Your NameRonaldo <ronal2do@gmail.com>
2016-07-28 13:13:29 -0300

    videos e aviso 21

--- resources/views/pages/video.blade.php
+++ resources/views/pages/video.blade.php
@@ -42,73 +42,42 @@
                 </div>   
               </div>
               <div class="column">
+
+              <div class="card is-fullwidth">
+                  <header class="card-header">
+                    <p class="card-header-title">
+                      Vídeos
+                    </p>
+                  </header>
+
                 @forelse ($videos as $v)
-                                            <a href="/video/{{ $v->slug }}">
-                                                <li>
-                                                    <img src="http://img.youtube.com/vi/{{ $v->url }}/2.jpg" class="profile-img pull-left">
-                                                    <div class="message-block">
-                                                        <div><span class="username">{{ $v->titulo }}</span> 
-                                                        </div>
-                                                        
-                                                    </div>
-                                                </li>
-                                            </a>
-                                            @empty
-                                            @endforelse
-              </div>
-              
-            </div>
-
-
-
-
-                <div class="container-fluid">
-                    <div class="side-body padding-top">
-                        
-                        <div class="row  no-margin-bottom">
-                            <div class="col-sm-8 col-xs-12">
-                                <div class="row">
-                                    <div class="col-xs-12">                       
-                                        <div class="embed-responsive embed-responsive-16by9">
-                                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/{{ $video->url }}?rel=0"></iframe>
-                                        </div>                                   
-                                        
+                
+                <div class="card-content">
+                    <div class="content">
+                        <a href="/video/{{ $v->slug }}">
+                            <li>
+                                <img src="http://img.youtube.com/vi/{{ $v->url }}/2.jpg" class="profile-img pull-left">
+                                <div class="message-block">
+                                    <div><span class="username">{{ $v->titulo }}</span> 
                                     </div>
+                                    
                                 </div>
-                            </div>
-
-                            <div class="col-sm-4 col-xs-12">
-                                <div class="card card-success">
-                                    <div class="card-header">
-                                        <div class="card-title">
-                                            <div class="title">Vídeos</div>
-                                        </div>
-                                        <div class="clear-both"></div>
-                                    </div>
-                                    <div class="card-body no-padding">
-                                        <ul class="message-list">
-                                            @forelse ($videos as $v)
-                                            <a href="/video/{{ $v->slug }}">
-                                                <li>
-                                                    <img src="http://img.youtube.com/vi/{{ $v->url }}/2.jpg" class="profile-img pull-left">
-                                                    <div class="message-block">
-                                                        <div><span class="username">{{ $v->titulo }}</span> 
-                                                        </div>
-                                                        
-                                                    </div>
-                                                </li>
-                                            </a>
-                                            @empty
-                                            @endforelse
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                            </li>
+                        </a>
                     </div>
                 </div>
 
+                @empty
+                @endforelse
 
+
+                  </div>
+              </div>
+
+
+
+              
+            </div>
       </div>
     </section>
 
